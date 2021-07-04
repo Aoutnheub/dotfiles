0a378ab4f608055dc2b656452e2cf05257dc31f4
Aoutnheub <dev.aoutnheub@gmail.com>
2021-07-04 23:31:17 +0300

    updated script

--- scripts/tablet-settings.sh
+++ scripts/tablet-settings.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/bash
+
 xinput set-prop "HUION PenTablet Pen (0)" --type=float "Coordinate Transformation Matrix" 1.055 0 -0.04 0 1.12 -0.07 0 0 1
 xinput set-prop "HUION PenTablet Pen (0)" --type=float "libinput Tablet Tool Pressurecurve" 0 0 0 0 0.3 0.3 0.3 0.3
