1e1f7748946b0e5adcd8bb144bf5d32bfbb4c232
Aoutnheub <dev.aoutnheub@gmail.com>
2021-12-21 11:19:49 +0200

    owo

--- scripts/switch-to-monitor.sh
+++ scripts/switch-to-monitor.sh
@@ -1,4 +1,5 @@
 xrandr --output eDP1 --off
 xrandr --output HDMI1 --primary --mode 1920x1080
+xrandr --dpi 96
 wal -R
 change-background.sh
--- scripts/tablet-settings.sh
+++ scripts/tablet-settings.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 
-xinput set-prop "HUION PenTablet Pen (0)" --type=float "Coordinate Transformation Matrix" 1.055 0 -0.04 0 1.12 -0.07 0 0 1
-xinput set-prop "HUION PenTablet Pen (0)" --type=float "libinput Tablet Tool Pressurecurve" 0 0 0 0 0.3 0.3 0.3 0.3
+xinput set-prop "HUION PenTablet stylus" --type=float "Coordinate Transformation Matrix" 1.055 0 -0.04 0 1.12 -0.07 0 0 1
+xinput set-prop "HUION PenTablet stylus" --type=float "libinput Tablet Tool Pressurecurve" 0 0 0 0 0.3 0.3 0.3 0.3
+stylus=$(xsetwacom list dev | sed -r "s/([^\s])\s+/\1 /g" | cut -d " " -f 6 | tail -1)
+xsetwacom set $stylus Button 2 3
+xsetwacom set $stylus Button 3 2
