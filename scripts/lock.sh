3304adbe43508e3be746d2ecde10b8093d19345e
Aoutnheub <dev.aoutnheub@gmail.com>
2022-06-29 00:05:15 +0300

    better lockscreen

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
-# icon="$HOME/Pictures/icons/lock-screen.png"
-# create a temp file
 img=$(mktemp /tmp/XXXXXXXXXXXXX.png)
-# Take a screenshot of current desktop
-import -silent -window root $img 
-# Pixelate the screenshot
+import -silent -window root $img
 convert $img -scale 10% -scale 1000% $img
-# Alternatively, blur the screenshot (slow!)
 # convert $img -blur 1,5 $img
-# Add the lock image
-# convert $img $icon -gravity center -composite $img
-# Run i3lock with custom background
-i3lock -u -i $img
-# Remove the tmp file
+i3lock -i $img -k \
+    --indicator \
+    --ind-pos="x+41:y+h-41" \
+    --radius=25 \
+    --ring-width=5 \
+    --line-uses-inside \
+    --time-pos="ix+41:iy+14" \
+    --time-align 1 \
+    --verif-pos="ix+35:iy-34" \
+    --verif-align 2 \
+    --verif-text="Verifying..." \
+    --wrong-pos="ix+24:iy-34" \
+    --wrong-align 2 \
+    --wrong-text="Failure!" \
+    --inside-color="00000000" \
+    --ring-color="ffffffff" \
+    --separator-color="00000000" \
+    --insidever-color="00000000" \
+    --insidewrong-color="d23c3dff" \
+    --ringver-color="ffffffff" \
+    --ringwrong-color="ffffffff" \
+    --keyhl-color="d23c3dff" \
+    --bshl-color="d23c3dff" \
+    --time-color="ffffffff" \
+    --verif-color="ffffffff" \
+    --wrong-color="d23c3dff" \
+    --wrong-font="sans-serif" \
+    --verif-font="sans-serif" \
+    --time-font="sans-serif" \
+    --date-str "" \
+    --time-size="32" \
+    --verif-size="12" \
+    --wrong-size="12"
 rm $img
