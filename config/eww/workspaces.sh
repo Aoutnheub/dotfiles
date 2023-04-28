15886f9ae962fee219a252eb520e67c0aee7be40
Aoutnheub <dev.aoutnheub@gmail.com>
2023-04-28 23:55:25 +0300

    fixed bar

--- config/eww/workspaces.sh
+++ config/eww/workspaces.sh
@@ -3,16 +3,16 @@
 readonly -a 'ws_names=("I" "II" "III" "IV" "V" "VI" "VII" "VIII" "IX" "X")'
 
 workspaces() {
-    ws_active=$(bspc query -D -d .focused.local --names)
-    ws_occupied=$(bspc query -D -d .occupied.local --names)
+    ws_focused=$(bspc query -D -d .focused.local --names)
+    ws_occupied=$(bspc query -D -d .occupied.local --names | tr '\n' ' ')
 
     declare -a ws_arr
 
     for (( i=0; i < 10; i++ )) do
         name=${ws_names[$i]}
-        if [[ $ws_active == $name ]]; then
+        if [[ $ws_focused == $name ]]; then
             ws_arr[$i]="(button :onclick \"bspc desktop -f $name.local\" (label :class \"offset-icon active-ws\" :text \"\"))"
-        elif [[ $(echo $ws_occupied | grep $name) != "" ]]; then
+        elif [[ $(echo " $ws_occupied " | grep " $name ") != "" ]]; then
             ws_arr[$i]="(button :onclick \"bspc desktop -f $name.local\" (label :class \"offset-icon occupied-ws\" :text \"\"))"
         else
             ws_arr[$i]="(button :onclick \"bspc desktop -f $name.local\" (label :class \"offset-icon\" :text \"\"))"
