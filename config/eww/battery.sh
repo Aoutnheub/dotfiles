cfed26296097c9c2cbca927557cf04f9d54c1d43
Aoutnheub <dev.aoutnheub@gmail.com>
2023-04-29 10:35:06 +0300

    fixed bar

--- config/eww/battery.sh
+++ config/eww/battery.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-b=upower -i $(upower -e | grep BAT) | grep percentage | cut -d ' ' -f 15 | cut -d '%' -f 1
+b=$(upower -i $(upower -e | grep BAT) | grep percentage | cut -d ' ' -f 15 | cut -d '%' -f 1)
 
-if [[ $b != "100" ]]; then
+if [[ $b == "100" ]]; then
     echo "F"
 else
     echo $b
